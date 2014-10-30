9a13c0ecd22bf8cd1481edd904d82dc62bfc92a6
harmonicnewmedia <harmonicnewmedia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-30 06:57:49 +0000

    Added a shortcode to embed the v3 graph.
    git-svn-id: https://plugins.svn.wordpress.org/share-link/trunk@1016439 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- frontend/price-graph.php
+++ frontend/price-graph.php
@@ -1,4 +1,5 @@
 <?php
+add_filter('the_content','renderGraph3');
 add_filter('the_content','renderGraph');
 
 function renderGraph($content) {
@@ -25,6 +26,36 @@ function renderGraph($content) {
         $script = "<script type=\"text/javascript\" src=\"".$url."\"></script><div id=\"sharelink-graph-".$symbol."\" style=\"height: 200px;\"></div>";
 
         $replace = "[sharelink-graph".$param."]";
+        return str_replace($replace,$script,$content);
+    } else {
+        return $content;
+    }
+}
+
+function renderGraph3($content) {
+    $sharelink = new ShareLink();
+    $details = $sharelink->getLicenseDetails();
+
+    $key = $details["license"];
+    $url = SHARELINK_SOURCE.$key."/graph?version=3";
+
+    $matches = Array();
+    $pattern = "/\[sharelink-graph-3(.*)\]/";
+    $match_count = preg_match($pattern, $content, $matches);
+
+    if ($match_count != 0) {
+        $param = $matches[1];
+        $symbol = trim($matches[1]);
+
+        if ($symbol == "") {
+            $symbol = $details["symbol"];
+        }
+
+        $url = $url."&symbol=".$symbol;
+
+        $script = "<script type=\"text/javascript\" src=\"".$url."\"></script><div id=\"sharelink-graph-".$symbol."\" style=\"height: 360px; width: 600px;\"></div>";
+
+        $replace = "[sharelink-graph-3".$param."]";
         return str_replace($replace,$script,$content);
     } else {
         return $content;
