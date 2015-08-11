cbcb05184f80317d70e1a3598ac38e4641d5acc5
harmonicnewmedia <harmonicnewmedia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-11 06:27:23 +0000

    Release 1.2.3.
    git-svn-id: https://plugins.svn.wordpress.org/share-link/tags/1.2.3@1217905 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- frontend/price-graph.php
+++ frontend/price-graph.php
@@ -2,62 +2,81 @@
 add_filter('the_content','renderGraph3');
 add_filter('the_content','renderGraph');
 
-function renderGraph($content) {
+function sharelinkGenerateGraphEmbed($symbol = '', $version = '1') {
     $sharelink = new ShareLink();
     $details = $sharelink->getLicenseDetails();
 
-    $key = $details["license"];
-    $url = SHARELINK_SOURCE.$key."/graph?version=1";
+    if ($symbol == "") {
+        $symbol = $details["stock"];
+    }
 
+    $key = $details["license"];
+    $url = SHARELINK_SOURCE.$key."/graph?version=" . $version;
+    $url = $url."&symbol=".$symbol;
+
+    $script = "<script type=\"text/javascript\" src=\"".$url."\"></script><div id=\"sharelink-graph-".$symbol."\" style=\"height: 200px;\"></div>";
+
+    return $script;
+}
+
+function renderGraph($content) {
     $matches = Array();
     $pattern = "/\[sharelink-graph(.*)\]/";
-    $match_count = preg_match($pattern, $content, $matches);
+    $match_count = preg_match_all($pattern, $content, $matches);
 
     if ($match_count != 0) {
-        $param = $matches[1];
-        $symbol = trim($matches[1]);
+        foreach ($matches[1] as $param) {
+            $symbol = trim($param);
 
-        if ($symbol == "") {
-            $symbol = $details["symbol"];
+            $script = sharelinkGenerateGraphEmbed($symbol, '1');
+
+            $replace = "[sharelink-graph".$param."]";
+            $content = str_replace($replace,$script,$content);
         }
 
-        $url = $url."&symbol=".$symbol;
-
-        $script = "<script type=\"text/javascript\" src=\"".$url."\"></script><div id=\"sharelink-graph-".$symbol."\" style=\"height: 200px;\"></div>";
-
-        $replace = "[sharelink-graph".$param."]";
-        return str_replace($replace,$script,$content);
+        return $content;
     } else {
         return $content;
     }
 }
 
 function renderGraph3($content) {
-    $sharelink = new ShareLink();
-    $details = $sharelink->getLicenseDetails();
-
-    $key = $details["license"];
-    $url = SHARELINK_SOURCE.$key."/graph?version=3";
-
     $matches = Array();
     $pattern = "/\[sharelink-graph-3(.*)\]/";
-    $match_count = preg_match($pattern, $content, $matches);
+    $match_count = preg_match_all($pattern, $content, $matches);
 
     if ($match_count != 0) {
-        $param = $matches[1];
-        $symbol = trim($matches[1]);
+        foreach ($matches[1] as $param) {
+            $symbol = trim($param);
 
-        if ($symbol == "") {
-            $symbol = $details["symbol"];
+            $script = sharelinkGenerateGraphEmbed($symbol, '3');
+
+            $replace = "[sharelink-graph-3".$param."]";
+            $content = str_replace($replace,$script,$content);
         }
 
-        $url = $url."&symbol=".$symbol;
-
-        $script = "<script type=\"text/javascript\" src=\"".$url."\"></script><div id=\"sharelink-graph-".$symbol."\" style=\"height: 360px; width: 600px;\"></div>";
-
-        $replace = "[sharelink-graph-3".$param."]";
-        return str_replace($replace,$script,$content);
+        return $content;
     } else {
         return $content;
     }
 }
+
+function shareShortcodeGraph($version, $atts) {
+    $atts = shortcode_atts( array(
+        'symbol' => ''
+    ), $atts );
+
+    $res = sharelinkGenerateGraphEmbed($atts['symbol'], $version);
+
+    return $res;
+}
+
+function shareShortcodeGraph1($atts) {
+    return shareShortcodeGraph('1', $atts);
+}
+function shareShortcodeGraph3($atts) {
+    return shareShortcodeGraph('3', $atts);
+}
+
+add_shortcode( 'sharelink-graph', 'shareShortcodeGraph1' );
+add_shortcode( 'sharelink-graph-3', 'shareShortcodeGraph3' );
--- frontend/price-html.php
+++ frontend/price-html.php
@@ -1,41 +1,79 @@
 <?php
 add_filter('the_content','renderHtml');
 
-function renderHtml($content) {
+function shareLinkGenerateHtmlEmbed($type, $div = '') {
     $sharelink = new ShareLink();
     $details = $sharelink->getLicenseDetails();
-
     $key = $details["license"];
 
+    if (empty($div)) {
+        $div = "sharelink-" . $type;
+    }
+
+    $url = SHARELINK_SOURCE.$key."/html?type=".$type;
+    $url = $url."&div=".$div;
+
+    $script = "<script type=\"text/javascript\" src=\"".$url."\"></script><div id=\"".$div."\"></div>";
+
+    return $script;
+}
+
+function renderHtml($content) {
     $matches = Array();
     $pattern = "/\[sharelink-(.*)\]/";
-    $match_count = preg_match($pattern, $content, $matches);
+    $match_count = preg_match_all($pattern, $content, $matches);
 
     if ($match_count != 0) {
-        $param = $matches[1];
+        foreach ($matches[1] as $param) {
+            if ($param == "graph" || $param == "graph-3" || $param == "asx") {
+                continue;
+            }
 
-        $items = trim($matches[1]);
+            $items = trim($param);
 
-        if (strpos($items," ") !== false) {
-            list($type,$div) = explode(" ",$details);
-        } else {
-            $type = $items;
+            if (strpos($items," ") !== false) {
+                list($type,$div) = explode(" ",$details);
+            } else {
+                $type = $items;
+            }
+
+            if (!isset($div)) {
+                $div = '';
+            }
+
+            $script = shareLinkGenerateHtmlEmbed($type, $div);
+            $replace = "[sharelink-".$param."]";
+            $content = str_replace($replace,$script,$content);
+            unset($div);
         }
 
-        $url = SHARELINK_SOURCE.$key."/html?type=".$type;
-
-        if (!isset($div) || $div == "") {
-            $div = "sharelink-".$type;
-        }
-
-        $url = $url."&div=".$div;
-
-        $script = "<script type=\"text/javascript\" src=\"".$url."\"></script><div id=\"".$div."\"></div>";
-
-        $replace = "[sharelink-".$param."]";
-        return str_replace($replace,$script,$content);
+        return $content;
     } else {
         return $content;
     }
 }
+
+function shareShortcodeHtml($type, $atts) {
+    $atts = shortcode_atts( array(
+        'div' => '',
+    ), $atts );
+
+    $res = shareLinkGenerateHtmlEmbed($type, $atts['div']);
+
+    return $res;
+}
+
+function shareShortcodeHtmlBox($atts) {
+    return shareShortcodeHtml('box', $atts);
+}
+function shareShortcodeHtmlStrip($atts) {
+    return shareShortcodeHtml('strip', $atts);
+}
+function shareShortcodeHtmlTable($atts) {
+    return shareShortcodeHtml('table', $atts);
+}
+
+add_shortcode( 'sharelink-box', 'shareShortcodeHtmlBox' );
+add_shortcode( 'sharelink-strip', 'shareShortcodeHtmlStrip' );
+add_shortcode( 'sharelink-table', 'shareShortcodeHtmlTable' );
 
